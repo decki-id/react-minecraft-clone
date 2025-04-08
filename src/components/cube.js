40e6d2b09c882291663d61c6d9b1ab99a7cd368d
decki-id <d.herdiawan.s@gmail.com>
2025-04-08 21:33:42 +0700

    Refactor indentations & comment out console.log().

--- src/components/cube.js
+++ src/components/cube.js
@@ -3,54 +3,49 @@ import { useStore } from "../hooks/useStore"
 import * as textures from "../images/textures"
 
 export const Cube = ({ position, texture }) => {
-    const
-        [ref] = useBox(() => ({
-            type: "Static",
-            position
-        })),
-        [addCube, removeCube] = useStore((state) => [state.addCube, state.removeCube]),
-        activeTexture = textures[texture + "Texture"],
-        click = (e) => {
-            e.stopPropagation()
-            const
-                clickedFace = Math.floor(e.faceIndex / 2),
-                { x, y, z } = ref.current.position
-            if (e.altKey) {
-                removeCube(x, y, z)
-                return
-            }
-            else if (clickedFace === 0) {
-                addCube(x + 1, y, z)
-                return
-            }
-            else if (clickedFace === 1) {
-                addCube(x - 1, y, z)
-                return
-            }
-            else if (clickedFace === 2) {
-                addCube(x, y + 1, z)
-                return
-            }
-            else if (clickedFace === 3) {
-                addCube(x, y - 1, z)
-                return
-            }
-            else if (clickedFace === 4) {
-                addCube(x, y, z + 1)
-                return
-            }
-            else if (clickedFace === 5) {
-                addCube(x, y, z - 1)
-                return
-            }
-        }
+  const [ref] = useBox(() => ({ type: "Static", position }))
+  const [addCube, removeCube] = useStore((state) => [state.addCube, state.removeCube])
+  const activeTexture = textures[texture + "Texture"]
+  const click = (e) => {
+    e.stopPropagation()
+    const clickedFace = Math.floor(e.faceIndex / 2)
+    const { x, y, z } = ref.current.position
+    if (e.altKey) {
+      removeCube(x, y, z)
+      return
+    }
+    else if (clickedFace === 0) {
+      addCube(x + 1, y, z)
+      return
+    }
+    else if (clickedFace === 1) {
+      addCube(x - 1, y, z)
+      return
+    }
+    else if (clickedFace === 2) {
+      addCube(x, y + 1, z)
+      return
+    }
+    else if (clickedFace === 3) {
+      addCube(x, y - 1, z)
+      return
+    }
+    else if (clickedFace === 4) {
+      addCube(x, y, z + 1)
+      return
+    }
+    else if (clickedFace === 5) {
+      addCube(x, y, z - 1)
+      return
+    }
+  }
 
-    console.log("activeTexture", activeTexture)
+  // console.log("activeTexture", activeTexture)
 
-    return (
-        <mesh onClick={click} ref={ref}>
-            <boxGeometry attach="geometry" />
-            <meshStandardMaterial map={activeTexture} attach="material" />
-        </mesh>
-    )
+  return (
+    <mesh onClick={click} ref={ref}>
+      <boxGeometry attach="geometry" />
+      <meshStandardMaterial map={activeTexture} attach="material" />
+    </mesh>
+  )
 }
