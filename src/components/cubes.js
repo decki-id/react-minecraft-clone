c744e3a146e4cef5db2e69ee12d72db9b31ff9fe
decki-id <d.herdiawan.s@gmail.com>
2025-04-08 21:32:51 +0700

    Rearrange import list, refactor indentations, & remove console.log().

--- src/components/cubes.js
+++ src/components/cubes.js
@@ -1,10 +1,9 @@
-import { useStore } from "../hooks/useStore"
 import { Cube } from "./cube"
+import { useStore } from "../hooks/useStore"
 
 export const Cubes = () => {
-    const [cubes] = useStore((state) => [state.cubes])
-    console.log(cubes)
-    return cubes.map(({ key, pos, texture }) => {
-        return (<Cube key={key} position={pos} texture={texture} />)
-    })
+  const [cubes] = useStore((state) => [state.cubes])
+  return cubes.map(({ key, pos, texture }) => {
+    return (<Cube key={key} position={pos} texture={texture} />)
+  })
 }
